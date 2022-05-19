fef0b8ad6828e37a743efff4407f3b2fb22e3d22
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-19 15:16:13 -0300

    add aula10 python

---
+++ Aula_10/01_Aula_10.py
@@ -0,0 +1,18 @@
+"""
+nome = str(input('Qual seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+print(f'Bom dia {nome}!')
+
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'A sua média foi {m:.1f}')
+if m >= 6.0:
+    print('Sua média foi boa, parabéns!')
+else:
+    print('Sua média foi ruim. Estude Mais!')
+"""
