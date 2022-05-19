b21e7b979537fabbf0725defd6f70c18cd3a36c5
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-19 15:17:41 -0300

    add aula14 python

---
+++ Aula_14/01_Aula_14.py
@@ -0,0 +1,28 @@
+c = 1
+while c < 10:
+    print(c)
+    c += 1
+print('FIM')
+
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('FIM')
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+print('FIM')
+
+n = 1
+p = i = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            p += 1
+        else:
+            i += 1
+print(f'Par: {p}\n'
+      f'Ímpar: {i}')
