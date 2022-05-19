6cead5aeee414bf5e9b516f38a9a654b15619a7d
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-19 15:11:31 -0300

    add aula07 python

---
+++ Aula_07/01_Aula_07.py
@@ -0,0 +1,16 @@
+"""
++ adição
+- subtração
+* multiplicação
+/ divisão
+
+** potência (outra forma de usar: pow(x, x))
+// divisão exata
+% módulo (resto da divisão)
+
+Ordem de Precedência
+1: ()
+2: **
+3: * / // %
+4: + -
+"""
