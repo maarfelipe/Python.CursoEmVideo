0db81a9f0ba7d5f57e006248863f63c0ec2152cf
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-05-23 07:54:00 -0300

    add aula22 python

---
+++ Aula_22/numeros.py
@@ -0,0 +1,7 @@
+from Aula_22.uteis import numeros
+
+num = int(input('Digite um valor: '))
+print(f'O número é: {num}\n'
+      f'Fatorial: {numeros.fatorial(num)}\n'
+      f'Dobro: {numeros.dobro(num)}\n'
+      f'Triplo: {numeros.triplo(num)}')
